c2832aec695a8ff4038ec25a4222debeb527ac36
EC <erik.cameron@gmail.com>
2018-06-12 20:41:46 -0500

    licenses

--- mix.exs
+++ mix.exs
@@ -24,18 +24,17 @@ defmodule Maxine.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       {:benchee, "~> 0.11", only: :dev},
-      {:dialyxir, "~> 0.5", only: [:dev], runtime: false}
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
   
   defp package do 
     %{
-      licences: ["MIT"],
+      licenses: ["MIT"],
       maintainers: ["Erik Cameron"],
-      links: ["GitHub" => "https://github.com/erikcameron/maxine"]
+      links: %{"GitHub" => "https://github.com/erikcameron/maxine"}
     }
   end
 end
