f51d865720bea4df4191cca169239d460393538b
EC <erik.cameron@gmail.com>
2018-06-14 14:28:59 -0500

    bump version

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Maxine.MixProject do
       name: "Maxine",
       app: :maxine,
       description: "State machines as data for Elixir",
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
